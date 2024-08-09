57312f648bfa2cf251b139e3a60ba436333b6d2d
hamza <hamza@exceev.com>
2024-08-09 16:55:43 +0200

    Add sorry-cypress config + improvment

--- currents.config.js
+++ currents.config.js
@@ -1,6 +1,10 @@
 // currents.config.js
 module.exports = {
-  projectId: 'yyy', // the projectId, can be any values for sorry-cypress users
-  recordKey: 'xxx', // the record key, can be any value for sorry-cypress users
-  cloudServiceUrl: 'http://localhost:1234', // Sorry Cypress users - set the director service URL
+  // projectId should match the projectId value used in Sorry-Cypress.
+  projectId: 'cy-sorry-cypress-dashboard',
+  // Record key, can be any value.
+  recordKey: 'cy-key',
+  // Director service URL
+  cloudServiceUrl:
+    process.env.SORRY_CYPRESS_DIRECTOR_URL || 'http://localhost:1234',
 };
